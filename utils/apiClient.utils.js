bc034116dbe60686d78236abd11e1edb177f6358
Malek <malek_tatin@yahoo.com>
2024-12-18 17:25:41 +0100

    update

--- utils/apiClient.utils.js
+++ utils/apiClient.utils.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 
 // Create an Axios instance
 const apiClient = axios.create({
-  baseURL: process.env.Root_URL_API, 
+  baseURL: 'https://api.coingecko.com/api/v3', 
   headers: {
-    'x-cg-demo-api-key': process.env.COINGECKO_API_KEY, 
+    'x-cg-demo-api-key': 'CG-1LYdPTaRpyfLAjCmt72k54jP', 
   }
 });
 
