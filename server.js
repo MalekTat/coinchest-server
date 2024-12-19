e568289699083842984adf318c97f2b8abb4155f
Malek <malek_tatin@yahoo.com>
2024-12-19 22:29:12 +0100

    cron

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require("./app");
 
-//const cron = require('node-cron');
-//const { processAlerts } = require('./services/alerts.services');
+const cron = require('node-cron');
+const { processAlerts } = require('./services/alerts.services');
 
 
-// Schedule the alerts processor to run every 5 minutes    ( */30 * * * * * )   30sec
-//cron.schedule('*/1 * * * *', async () => {
-//  console.log('Running scheduled alerts processing...');
-//  await processAlerts();
-//}); 
+// Schedule the alerts processor to run every 5 minutes    ( */1 * * * * )   30sec  
+cron.schedule('*/10 * * * * *', async () => {
+  console.log('Running scheduled alerts processing...');
+  await processAlerts();
+}); 
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
